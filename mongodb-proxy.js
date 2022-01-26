0c3efc7a05e591ecb476dfa1ccedb708f6a38a54
ckwok1 <ckwok1@jaguarlandrover.com>
2022-01-26 20:51:10 +0000

    update mulitple db with empty collection check

--- mongodb-proxy.js
+++ mongodb-proxy.js
@@ -288,12 +288,6 @@ function parseQuery(query, substitutions)
 
 function runAggregateQuery( requestId, queryId, body, queryArgs, res, next )
 {
-  //console.log("[Debug] queryId:")
-  //console.log(queryId)
-  //console.log("[Debug] queryArgs:")
-  //console.log(queryArgs)
-  //console.log("[Debug] body.targets:")
-  //console.log("[Debug] body.db.url -:" + body.db.url)
   MongoClient.connect(body.db.url, function(err, client)
   {
     if ( err != null )
@@ -308,32 +302,15 @@ function runAggregateQuery( requestId, queryId, body, queryArgs, res, next )
 
       const db = client.db(body.db.db);
 
-      // Empty collection will assgin an invaild collection name (return empty result only, no error will trigger restart)
-      if(queryArgs.collection == ""){
-        queryArgs.collection = "INVAILD_COLLECTION";
-      }
-
       // Get the documents collection
       if(queryArgs.collection.split(".").length > 1){
-        //                0            1
-        // format: db.<database>.<collection>.aggregate({})
-        queryArgs.collection = queryArgs.collection.split(".")[1];
+        //extract collection name, aviod triming off collection name with "." in it (consecutive "...") will treat as one "."
+        queryArgs.collection = queryArgs.collection.split(".").slice(1).filter(Boolean).join(".")
       }
 
       const collection = db.collection(queryArgs.collection);
       logQuery(queryArgs.pipeline, queryArgs.agg_options)
       var stopwatch = new Stopwatch(true)
-
-      //console.log("[Debug] queryId -: " + queryId);
-      //console.log("[Debug] dbLookUp -: " + dbLookUp);
-      //console.log("[Debug] body.targets.db -: '" + dbLookUp + "'" )
-      //console.log("[Debug] body.db.db -: '" + body.db.db + "'")
-      //console.log("[Debug] queryArg.collection -: '" + queryArgs.collection + "'")
-      //console.log("-------------------------------------")
-      //console.log(queryArgs.pipeline)
-      //console.log("-------------------------------------")
-      //console.log(queryArgs.agg_options)
-      //console.log("-------------------------------------")
 
       collection.aggregate(queryArgs.pipeline, queryArgs.agg_options).toArray(function(err, docs)
         {
